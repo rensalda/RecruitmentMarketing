dd0c895d751142a8fb075f86359a408d65d01682
Renny Saldanha <renny.saldanha@publicissapient.com>
2023-11-16 15:34:56 -0500

    Added yyyy to date format

--- SmartJobs for Datorama.py
+++ SmartJobs for Datorama.py
@@ -18,7 +18,7 @@ for input_file in files:
     df = pd.read_csv(input_file, skip_blank_lines=True)
     output_file = output_file.append(df)
 output_file.reset_index(inplace=True, drop=True)
-output_file.to_csv(folder + 'SmartJobs_YTD_report_01.01-06.30.csv')
+# output_file.to_csv(folder + 'SmartJobs_YTD_report_01.01-06.30.csv')
 
 
-
+output_file.to_csv(folder + 'SmartJobs_YTD_report_01.01_2022-06.30_2022.csv')
